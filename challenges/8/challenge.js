efca09ca7b71bc2c66b10aa6ea251a7044bba6d4
Gabriela Fonseca <gabriela.fonseca@idwall.co>
2020-11-17 10:00:59 -0300

    added combinate challenge

---
+++ challenges/8/challenge.js
@@ -0,0 +1,21 @@
+/*
+ * Soma Combinacional
+ */
+
+/* ENUNCIADO
+ *
+ *  Você deve escrever uma função que retorne todas as combinações de soma possíveis para um determinado número, 
+ * como por exemplo:
+ * 
+ *  combinate([2, 3, 5], 8) deve retornar
+ * 
+ *  [
+        [2,2,2,2],
+        [2,3,3],
+        [3,5]
+    ]
+ */
+
+const combinate = (set, target) => {}
+
+module.exports = combinate
